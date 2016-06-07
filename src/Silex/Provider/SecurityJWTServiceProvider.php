edc457be80a140587ba2fd016316cafdad06da44
Alexandre Dupuy <alex.dpy@gmail.com>
2016-06-07 16:17:39 +0200

    update to silex 2

--- src/Silex/Provider/SecurityJWTServiceProvider.php
+++ src/Silex/Provider/SecurityJWTServiceProvider.php
@@ -12,8 +12,8 @@ use Evaneos\JWT\Util\JWTDecoder;
 use Evaneos\JWT\Util\JWTEncoder;
 use Evaneos\JWT\Util\JWTUserBuilder;
 use Evaneos\JWT\Util\SecurityUserConverter;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 
 class SecurityJWTServiceProvider implements ServiceProviderInterface
 {
@@ -23,71 +23,69 @@ class SecurityJWTServiceProvider implements ServiceProviderInterface
      * This method should only be used to configure services and parameters.
      * It should not get services.
      */
-    public function register(Application $app)
+    public function register(Container $app)
     {
-        $app['security.jwt_retrieval.authorization_bearer.strategy'] = $app->share(function () {
+        $app['security.jwt_retrieval.authorization_bearer.strategy'] = function () {
             return new AuthorizationBearerStrategy();
-        });
+        };
 
-        $app['security.jwt_retrieval.query_parameter.strategy'] = $app->share(function () {
+        $app['security.jwt_retrieval.query_parameter.strategy'] = function () {
             return new QueryParameterStrategy();
-        });
+        };
 
-        $app['security.jwt_retrieval.chain.strategy'] = $app->share(function () use ($app) {
-            return new ChainStrategy([
-                $app['security.jwt_retrieval.authorization_bearer.strategy'],
-                $app['security.jwt_retrieval.query_parameter.strategy'],
-            ]);
-        });
-
-        $app['security.entry_point.jwt._proto'] = $app->protect(function () use ($app) {
-            return $app->share(function () {
-                return new JWTAuthenticationEntryPoint();
-            });
-        });
-
-        $app['security.authentication_listener.factory.jwt'] = $app->protect(function ($name, $options) use ($app) {
-
-            $app['security.authentication_provider.' . $name . '.jwt'] = $app->share(function () use ($app, $options) {
-                $encoder = new JWTEncoder($options['secret_key'], reset($options['allowed_algorithms']));
-                $decoder = new JWTDecoder($options['secret_key'], $options['allowed_algorithms']);
-                $converter = new SecurityUserConverter();
-                $userBuilder = new JWTUserBuilder($decoder, $encoder, $converter);
-
-                return new JWTAuthenticationProvider($userBuilder);
-            });
-
-            $app['security.authentication_listener.' . $name . '.jwt'] = $app->share(function () use ($app, $name, $options) {
-                $strategyName = isset($options['retrieval_strategy'])
-                    ? $options['retrieval_strategy']
-                    : 'authorization_bearer';
-
-                return new JWTListener(
-                    $app['security.token_storage'],
-                    $app['security.authentication_manager'],
-                    $app['security.jwt_retrieval.' . $strategyName . '.strategy']
-                );
-            });
-
-            $app['security.entry_point.' . $name . '.jwt'] = $app['security.entry_point.jwt._proto']($name, $options);
-
-            return array(
-                'security.authentication_provider.' . $name . '.jwt',
-                'security.authentication_listener.' . $name . '.jwt',
-                'security.entry_point.' . $name . '.jwt',
-                'pre_auth',
+        $app['security.jwt_retrieval.chain.strategy'] = function () use ($app) {
+            return new ChainStrategy(
+                [
+                    $app['security.jwt_retrieval.authorization_bearer.strategy'],
+                    $app['security.jwt_retrieval.query_parameter.strategy'],
+                ]
             );
-        });
-    }
+        };
 
-    /**
-     * Bootstraps the application.
-     *
-     * This method is called after all services are registered
-     * and should be used for "dynamic" configuration (whenever
-     * a service must be requested).
-     */
-    public function boot(Application $app)
-    {
+        $app['security.entry_point.jwt._proto'] = $app->protect(
+            function () use ($app) {
+                return function () {
+                    return new JWTAuthenticationEntryPoint();
+                };
+            }
+        );
+
+        $app['security.authentication_listener.factory.jwt'] = $app->protect(
+            function ($name, $options) use ($app) {
+
+                $app['security.authentication_provider.' . $name . '.jwt'] = function () use ($app, $options) {
+                    $encoder = new JWTEncoder($options['secret_key'], reset($options['allowed_algorithms']));
+                    $decoder = new JWTDecoder($options['secret_key'], $options['allowed_algorithms']);
+                    $converter = new SecurityUserConverter();
+                    $userBuilder = new JWTUserBuilder($decoder, $encoder, $converter);
+
+                    return new JWTAuthenticationProvider($userBuilder);
+                };
+
+                $app['security.authentication_listener.' . $name . '.jwt'] = function () use ($app, $name, $options) {
+                    $strategyName = isset($options['retrieval_strategy'])
+                        ? $options['retrieval_strategy']
+                        : 'authorization_bearer';
+
+                    return new JWTListener(
+                        $app['security.token_storage'],
+                        $app['security.authentication_manager'],
+                        $app['security.jwt_retrieval.' . $strategyName . '.strategy']
+                    );
+                };
+
+                $app['security.entry_point.' . $name . '.jwt'] = $app['security.entry_point.jwt._proto'](
+                    $name,
+                    $options
+                );
+
+                return array(
+                    'security.authentication_provider.' . $name . '.jwt',
+                    'security.authentication_listener.' . $name . '.jwt',
+                    'security.entry_point.' . $name . '.jwt',
+                    'pre_auth',
+                );
+            }
+        );
     }
 }
